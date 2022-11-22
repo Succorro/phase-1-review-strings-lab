a7f0c6b10181a595765e87936c69b6457bfd9bd0
Steven Gutierrez <stevogbmv@gmail.com>
2022-11-22 12:29:27 -0800

    commit tree 82bb41dd48e9f8642baeef2b20551ce5268203e1

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Steven'
+const welcomeMessage = ` Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
